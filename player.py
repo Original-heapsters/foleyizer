c44fab66bd6cd3e9227c970d59bc9a9e19d640d6
Russell <sell_nat@yahoo.com>
2018-07-21 15:14:12 -0700

    Added random filler and random player

--- player.py
+++ player.py
@@ -1,3 +1,5 @@
+import os
+import random
 import simpleaudio as sa
 
 class Player:
@@ -6,14 +8,28 @@ class Player:
         return
 
     def playWav(self, pathToWav=None):
-        if pathToWav is not None or pathToWav != '':
+        if pathToWav is not None and pathToWav != '':
             wave_obj = sa.WaveObject.from_wave_file(pathToWav)
             play_obj = wave_obj.play()
             play_obj.wait_done()
         else:
             return 'No audio path provided'
 
+    def playRandom(self, pathToFillerDir=None):
+        if pathToFillerDir is not None and pathToFillerDir != '':
+            files = []
+            for file in os.listdir(pathToFillerDir):
+                if file.endswith(".wav"):
+                    path = os.path.join(pathToFillerDir, file)
+                    files.append(path)
+            self.playWav(pathToWav=random.choice(files))
+
+
 if __name__ == '__main__':
-    pathToTestFile = 'wav_files/back2work.wav'
+    pathToTestFile = './wav_files/test.wav'
+    pathToFillers = './wav_files/filler/'
     play = Player(currentAudioPath=None)
+    print('Playing test file')
     play.playWav(pathToWav=pathToTestFile)
+    print('Playing random filler file')
+    play.playRandom(pathToFillerDir=pathToFillers)
