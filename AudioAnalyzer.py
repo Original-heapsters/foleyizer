650c5c0420443ac361d2e40fb8b02659b75eabc5
Russell <sell_nat@yahoo.com>
2018-07-22 14:33:57 -0700

    Finished up the audio analyzer portion...at least in an mvp state

--- AudioAnalyzer.py
+++ AudioAnalyzer.py
@@ -1,3 +1,4 @@
+import os
 import random
 from player import Player
 
@@ -6,7 +7,7 @@ class AudioAnalyzer:
         self.play = Player()
         return
 
-    def findDeadAreas(self):
+    def findDeadAreas(self, inputJson=None):
         dummyJson = {
         'path':'./wav_files/badness.wav',
         'title':'badness.wav',
@@ -69,48 +70,66 @@ class AudioAnalyzer:
         }
         ]
         }
-        originalAudio = dummyJson['path']
+
+        if not inputJson:
+            analysisJson = dummyJson
+        else:
+            analysisJson = inputJson
+
+        originalAudio = analysisJson['path']
+        audioCombinationCommand = 'ffmpeg -y -loglevel panic -i ' + originalAudio + ' '
+        filterCommand = '-filter_complex \"'
         finalMix = None
         intermediateMixes = []
-        counter = 0
-        for k,v in dummyJson.items():
+        counter = 1
+        startingLetter = 'b'
+        allLetters = ''
+        finalMix = './wav_files/mixed_audio.wav'
+        for k,v in analysisJson.items():
             if k == 'sections':
-                for section in dummyJson[k]:
+                for section in analysisJson[k]:
                     if section['isDead']:
-                        # print('The audio is dead: ' + str(section['isDead']) + ' starts at: ' + str(section['start']) + ' ends at: ' + str(section['end']))
                         duration = int(round(section['end'] - section['start']))
-                        # print('Duration is ' + str(duration))
-                        fillerChosen, iterations = self.findFittingFiller(desiredDuration=duration)
+                        fillerChosen, _ = self.findFittingFiller(desiredDuration=duration)
 
-                        print('Decided to play ' + fillerChosen + ' x' + str(iterations) + ' To fill ' + str(duration) + ' starting at ' + str(section['start']))
-                        intermediateMixes.append((fillerChosen,section['start']))
+                        audioCombinationCommand += '-i ' + fillerChosen + ' '
+                        filterCommand += '[' + str(counter) + ']adelay=' + str(section['start']*1000) + '|' + str(section['start']*1000) + '[' + startingLetter + '];'
+                        allLetters += '[' + startingLetter + ']'
+
                         counter += 1
+                        startingLetter = chr(ord(startingLetter) + 1)
 
-        finalMix = self.play.combineAudioFiles(src=originalAudio, fillersWithOffsets=intermediateMixes)
-        self.play.playWav(pathToWav=finalMix)
+        filterCommand += '[0]' + allLetters + 'amix=' + str(counter) + '\" -ar 44100 ' + finalMix
+        os.system(audioCombinationCommand + filterCommand)
 
+        return finalMix
+
+    def playMix(self, mixPath=None):
+        self.play.playWav(pathToWav=mixPath)
 
     def findFittingFiller(self, desiredDuration=0):
 
         pathToFillers = './wav_files/filler/'
         fillers = self.play.getListOfFillers(pathToFillerDir=pathToFillers)
         selectedFile = None
-        for fi in fillers:
-            print('.')
-            if desiredDuration > 0 and int(round(self.play.getWavLength(pathToWav=fi))) % desiredDuration == 0:
-                print('Found it! ' + fi)
-                selectedFile = fi
-                break
+        findRandom = True
+        if not findRandom:
+            for fi in fillers:
+                print('.')
+                if desiredDuration > 0 and int(round(self.play.getWavLength(pathToWav=fi))) % desiredDuration == 0:
+                    print('Found it! ' + fi)
+                    selectedFile = fi
+                    break
 
-        if not selectedFile:
-            print('No Dice')
+            if not selectedFile:
+                print('No Dice')
+                selectedFile = random.choice(fillers)
+        else:
             selectedFile = random.choice(fillers)
 
         return selectedFile, 1
 
-    def getLengthOfAudio(self, pathToAudio):
-        return random.randint(0,10)
-
 if __name__ == '__main__':
     analyzer = AudioAnalyzer()
-    analyzer.findDeadAreas()
+    finalMix = analyzer.findDeadAreas()
+    analyzer.playMix(mixPath=finalMix)
