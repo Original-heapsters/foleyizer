75a5c3f2fc0a20faf7745e56d271449e814671b2
Jeimmi <jeimmi.gomez@gmail.com>
2018-07-21 21:19:56 -0700

    everything up to plotTone works in main

--- spectrogram.py
+++ spectrogram.py
@@ -18,36 +18,44 @@ class Spectogram:
     def graphSetting(self, myAudio):
         if myAudio:
             samplingFreq, mySound = wavfile.read(myAudio)
+            print(samplingFreq)
             mySoundDataType = mySound.dtype
             print(mySoundDataType)
             mySound = mySound / (2.**15)
             mySoundShape = mySound.shape
             samplePoints = float(mySound.shape[0])
             print(samplePoints)
+            print(samplingFreq)
             return mySound, samplingFreq, samplePoints
 
-    def fileDuration(self, mySound, samplingFreq, samplePoints):
-        if mySound and samplingFreq:
-            signalDuration = mySound.shape[0]/samplingFreq
+    def fileDuration(self, mySound, samplingFreq):
+
+        if mySound.any():
+            print('dura')
+            print(mySound.shape[0])
+            signalDuration = float(mySound.shape[0])/samplingFreq
             print(signalDuration)
+            return signalDuration
 
     def soundChannel(self, mySound):
-        if mySound:
+        if mySound.any():
             if len(mySound.shape) > 1:
                 mySoundOneChannel = mySound[:,0]
+                return mySoundOneChannel
             else:
                 mySoundOneChannel = mySound
+                return mySoundOneChannel
 
 
     def plotTime(self, samplePoints,samplingFreq, mySoundOneChannel):
-        if samplePoints and samplingFreq and mySoundOneChannel:
+        if samplePoints and samplingFreq and mySoundOneChannel.any():
             timeArray = numpy.arange(0,samplePoints,1)
             timeArray = timeArray / samplingFreq
             timeArray = timeArray * 1000
             return timeArray
 
     def plotTone(self, timeArray, mySoundOneChannel):
-        if timeArray and mySoundOneChannel:
+        if timeArray.any() and mySoundOneChannel.any():
             #Plot the tone
             plt.plot(timeArray, mySoundOneChannel, color='G')
             plt.xlabel('Time (ms)')
@@ -102,7 +110,10 @@ if __name__ == '__main__':
     path = Spectogram()
     wav = path.chooseWav(pathToWav)
     sound, graph, points = path.graphSetting(wav)
-    mySound, samplingFreq, samplePoints = path.fileDuration(sound, graph, points)
+    dura = path.fileDuration(sound, graph)
+    chan = path.soundChannel(sound)
+    plotTime = path.plotTime(points,graph, chan)
+    tone= path.plotTone(plotTime, chan)
 
 
 
